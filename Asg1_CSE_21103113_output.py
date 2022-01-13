28c1571bb83c5fa8bb1a0d5e1e1a7314ced47a77
Rhythmkad108 <97380717+Rhythmkad108@users.noreply.github.com>
2022-01-13 08:44:44 +0530

    Add files via upload

---
+++ Asg1_CSE_21103113_output.py
@@ -0,0 +1,23 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+================== RESTART: C:/Python/Assignment 1 21103113.py =================
+<<<<<[Question 1]>>>>>---------- 
+Enter three numbers.
+First number is 20
+Second number is 30
+Third number is 40
+Average of the three numbers is: 30.0
+<<<<<[Question 2]>>>>>---------- 
+All money is in Dollars$
+Gross income=50000
+Number of Dependents=5
+Your Income Tax is: 5000.0
+<<<<<[Question 3]>>>>>---------- 
+Enter details. 
+Student iD:21103113
+Name:Rhythm Kad
+Gender(M for male,F for female,U for unknown):M
+Course Name:CSE
+CGPA:9.95
+['21103113', 'Rhythm Kad', 'M', 'CSE', '9.95']
